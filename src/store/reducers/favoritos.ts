ac326c8ec4b1a976571a598ece9672ad72f5f3d2
Elvaro-KSKSKS <100244045+Elvaro-KSKSKS@users.noreply.github.com>
2026-04-21 14:29:30 -0300

    redux implementado

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type favoritosState = {
+  itens: Produto[]
+}
+
+const initialState: favoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      const index = state.itens.findIndex((p) => p.id === produto.id)
+
+      if (index !== -1) {
+        state.itens.splice(index, 1)
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
